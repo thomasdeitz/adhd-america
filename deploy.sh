ba6d1015b02e10106acaee2127ef1573bccca274
Tom Deitz <info@thomasdeitz.com>
2018-05-02 01:07:58 -0500

    adjusts travis.yml

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,10 @@ CURRENT_REPO_SLUG=$(echo $TRAVIS_REPO_SLUG | cut -d "/" -f2 )
 CURRENT_COMMIT=`git rev-parse HEAD`
 
 echo "Targeting $TARGET repository"
-TARGET_URL=$TARGET_URL_CREDENTIALS
 
 echo "Cloning repository from $TARGET"
 cd ../
-git clone $TARGET_URL || exit 1
+git clone $TARGET_URL_CREDENTIALS || exit 1
 cd $TARGET || exit 1
 ls || exit 1
 
@@ -22,3 +21,23 @@ echo "Checking out dev from $TARGET"
 git checkout dev || exit 1
 ls || exit 1
 cd ../
+
+echo "Compiling new static content for $TARGET_URL_CREDENTIALS"
+harp compile $CURRENT_REPO_SLUG $TARGET || exit 1
+cd $TARGET
+
+echo "Commit updates"
+git add -A || exit 1
+git commit --allow-empty -m "Compiled content for $CURRENT_COMMIT" || exit 1
+
+echo "Pushing updates"
+git push --force --quiet "$TARGET_URL_WITH_CREDENTIALS" || exit 1
+
+cd ../
+
+echo "Cleaning up temp files"  
+rm -Rf $TARGET || exit 1
+rm -Rf $CURRENT_REPO_SLUG || exit 1
+
+echo "Deployed successfully."  
+exit 0
